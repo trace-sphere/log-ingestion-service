62436131f24a63af0c1c397e7eb0e135ee7f3f01
Priyabrat <105908418+the-priyabrat@users.noreply.github.com>
2026-07-30 23:52:34 +0530

    Added new response header Access-Control-Expose-Header as Current-Fetched-Time-Stamp for keeping record of latest updated ui

--- src/main/java/com/log/ingestion/log_ingestion_service/config/CorsConfig.java
+++ src/main/java/com/log/ingestion/log_ingestion_service/config/CorsConfig.java
@@ -3,28 +3,44 @@ package com.log.ingestion.log_ingestion_service.config;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
+import java.time.Instant;
 
-@Component
+@Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig implements Filter {
 
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
+    @Value("${cors.allowed.http.headers}")
+    private String allowedHeaders;
+
+    @Value("${cors.allowed.http.methods}")
+    private String allowedMethods;
+
+    @Value("${cors.allowed.client.origins}")
+    private String allowedOrigins;
+
+    @Value("${cors.allowed.http.exposed.headers}")
+    private String exposedHeaders;
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4300");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods",
-                "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization, TransactionId,locale,X-XSRF-TOKEN,X-Client-Type,X-Device-Fingerprint,X-Ip-Address");
 
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigins);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", allowedMethods);
+        response.setHeader("Access-Control-Allow-Headers", allowedHeaders);
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Current-Fetched-Time-Stamp", Instant.now().toString());
+        response.setHeader("Access-Control-Expose-Headers", exposedHeaders);
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
@@ -33,11 +49,6 @@ public class CorsConfig implements Filter {
     }
 
     public void init(FilterConfig filterConfig) {
-        // not needed
-    }
-
-    public void destroy() {
-        //not needed
     }
 
 }
